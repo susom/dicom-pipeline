42c1da05e532a355abbc6e28c5005d5675588716
eyl407 <eloh@stanford.edu>
2021-05-27 17:39:22 -0700

    file renamed

--- dags/sql/8_select_dcm_pass.sql
+++ dags/sql/8_select_dcm_pass.sql
@@ -1,1 +1,1 @@
-select distinct accession_number from dcm_qa where customer='Total Joint' and qa_pass='Y';
+select distinct accession_number from dcm_qa where customer='Total Joint' and qa_pass='Y' and trunc(qa_date)>trunc(sysdate-7)
